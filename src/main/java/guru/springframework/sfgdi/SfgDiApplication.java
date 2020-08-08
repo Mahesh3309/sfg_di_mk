27a9b6a0a48c4866155926a20f71e008404320b4
Mahesh Kharat <kharatm428@gmail.com>
2020-08-08 15:33:02 +0530

    MK404 DI implementation with @Service, @Controller and #Autowired annotations.

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
 import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PropertyInjectedController;
+import guru.springframework.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,7 +18,25 @@ public class SfgDiApplication {
 		String greeting=myController.sayHello();
 
 		System.out.println(greeting);
-		//System.out.println("SfgDiApplication.main");
+
+		System.out.println("------------MK404 Property Based Injection-------------");
+
+		PropertyInjectedController propertyInjectedController=(PropertyInjectedController) ctx.getBean("propertyInjectedController");
+
+		System.out.println(propertyInjectedController.getGreetings());
+
+		System.out.println("------------MK404 Setter Based Injection-------------");
+
+		SetterInjectedController setterInjectedController=(SetterInjectedController) ctx.getBean("setterInjectedController");
+
+		System.out.println(setterInjectedController.getGreetings());
+
+		System.out.println("------------MK404 Constructor Based Injection-------------");
+
+		ConstructorInjectedController constructorInjectedController= (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+
+		System.out.println(constructorInjectedController.getGreetings());
+
 	}
 
 }
